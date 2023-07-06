aa43cfd030d62c212424f6312f4293d001f036d2
Trixek24 <121521201+Trixek24@users.noreply.github.com>
2023-07-05 23:42:29 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-        
+    function startClassification()
+    {
+        navigator.mediaDevices.getUserMedia({ audio:true});
+        classifier = ml5.soundClassfier("https://teachablemachine.withgoogle.com/models/DIlYiObDM/model.json", modelReady);
+    }    
+
+    function modelReady()
+    {
+        classifier.classify(gotResults);
+    }
+
+    function gotResults(error, results)
+    {
+        console.log("got results");
+    }
